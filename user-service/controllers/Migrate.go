a04d61f20057b6db006dd767482b552235644b52
huynhhung30 <huynhhung30>
2024-03-07 12:15:49 +0700

    docker setup

--- user-service/controllers/Migrate.go
+++ user-service/controllers/Migrate.go
@@ -2,13 +2,14 @@ package controllers
 
 import (
 	"user_service/config"
+	"user_service/models"
 	"user_service/utils/functions"
 )
 
 // Auto migrate
 func Migrate() {
 	config.DB.AutoMigrate(
-	// models.UserModel{},
+		models.UserModel{},
 	// models.SocialInfoModel{},
 	)
 	functions.ShowLog("MigrateModel", "Success")
